e2b3b833669c6adbaaed2cad43a949475a1bcdb6
Nalankumar <Nalankumar@users.noreply.github.com>
2024-11-20 21:05:52 +0530

    email functionality updated

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,13 +1,34 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Form(){
+    const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(import.meta.env.VITE_YOUR_SERVICE_ID, import.meta.env.VITE_YOUR_TEMPLATE_ID, form.current, {
+        publicKey: import.meta.env.VITE_YOUR_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error);
+        },
+      );
+  };
+
     return(
-		<div id="contact" className="form-control mx-[5vw] md:mx-[20vw]">
+		<form ref={form} onSubmit={sendEmail}  className="form-control mx-[5vw] md:mx-[20vw]">
 			<label className="label">Name</label>
-			<input t ype="text" className="grow input input-bordered"/>
+			<input type="text" name="user_name" placeholder="Lily" className="grow input input-bordered"/>
 			<label className="label">Email</label>
-			<input t ype="text" className="grow input input-bordered"/>
+			<input type="text" name="user_email" placeholder="lily@gmail.com" className="grow input input-bordered"/>
 			<label className="label">Message</label>
-			<textarea placeholder="Write your message here..." className="textarea textarea-bordered textarea-sm w-full max-w-full"></textarea>
-  			<button className="btn my-[10px]">Contact me</button>
-		</div>
+			<textarea name="message" placeholder="Write your message here..." className="textarea textarea-bordered textarea-sm w-full max-w-full"></textarea>
+  			<button className="btn my-[10px]" type="submit" form="form"> Concact me</button>
+		</form>
 	);
 }
